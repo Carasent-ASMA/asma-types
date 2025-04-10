dcf4dda0c17b1ca42e6d16c48c0775236ad11c8c
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2025-04-10 23:52:22 +0300

    chore: ASMA-4351 Added new protected route for batch import of orgs

--- src/interfaces/enums.ts
+++ src/interfaces/enums.ts
@@ -364,6 +364,7 @@ export const ProtectedRoutesPaths = {
     organizations: 'organizations',
     organizationsNew: 'organizations/new',
     organizationsEdit: 'organizations/edit',
+    organizationsImport: 'organizations/import',
     //
     plans: 'plans',
     qnrModule: 'qnr-module',
