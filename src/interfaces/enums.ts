ebc53be6fe7c3a0038a0c6fc2aed9e99e3674dd9
Alexandr Doicov <alexandr.doicov@carasent.com>
2026-07-29 14:50:30 +0300

    feat(enums): Add releasePackages route to ProtectedRoutesPaths

--- src/interfaces/enums.ts
+++ src/interfaces/enums.ts
@@ -410,6 +410,7 @@ export const ProtectedRoutesPaths = {
     meetingSessionRoom: 'meeting-session/:roomSession',
 
     // revman
+    releasePackages: 'revman/release-packages',
     selectFeatures: 'revman/features',
     selectVersions: 'revman/multiversion',
 
